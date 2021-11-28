bef1a9c1b85276a76a0ffb8e7a48f8873aa197da
abiduddin <abidakash456@gmail.com>
2021-11-28 23:54:39 +0600

    nov 28

--- code-java/src/twenty/one/nov/Generic_bounded_types.java
+++ code-java/src/twenty/one/nov/Generic_bounded_types.java
@@ -1,25 +1,26 @@
 package twenty.one.nov;
 
-public class Generic_bounded_types {
-
-    public static class Stats<T> {
-        T[] nums;
-        public Stats(T[] nums) {
-            this.nums = nums;
-        }
-        // Return type double in all cases.
-        double average() {
-            double sum = 0.0;
-            for (T num : nums) {
-                sum += (int)num; // Error!!!
-            }
-            return sum / nums.length;
-        }
+class Stats<T> {
+    T[] nums;
+    public Stats(T[] nums) {
+        this.nums = nums;
     }
+    // Return type double in all cases.
+    double average() {
+        double sum = 0.0;
+        for (T num : nums) {
+            sum += (int) num; // Error!!!
+        }
+        return sum / nums.length;
+    }
+}
+
+public class Generic_bounded_types {
 
     public static void main(String[] args)
     {
-        Integer[] a = {1,2,3};
+        Integer[] a = {1,2,3,89};
         Stats<Integer> inn = new Stats<Integer>(a);
+        System.out.println(inn.average());
     }
 }
