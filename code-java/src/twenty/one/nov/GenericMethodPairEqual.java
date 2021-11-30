731d332539e02e927832328432a77870a54b9486
abiduddin <abidakash456@gmail.com>
2021-11-30 08:40:48 +0600

    nov 30 - generic method map

---
+++ code-java/src/twenty/one/nov/GenericMethodPairEqual.java
@@ -0,0 +1,30 @@
+package twenty.one.nov;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenericMethodPairEqual {
+    public static void main(String[] args) {
+        Map<Integer, String> p1 = new HashMap<Integer, String>();
+        p1.put(1, "one");
+        p1.put(2, "two");
+        Map<Integer, String> p2 = new HashMap<Integer, String>();
+        p2.put(1, "one");
+        p2.put(2, "two");
+
+        System.out.println(compare(p1, p2));
+        System.out.println(compareStream(p1, p2));
+    }
+
+    public static <K, V> boolean compare(Map<K, V> p1, Map<K, V> p2) {
+        return p1.equals(p2);
+    }
+
+    public static <K, V> boolean compareStream(Map<K, V> p1, Map<K, V> p2) {
+        if (p1.size() != p2.size())
+            return false;
+        return p1.entrySet().stream()
+                .allMatch(e->e.getValue().equals(p2.get(e.getKey())));
+    }
+
+}
