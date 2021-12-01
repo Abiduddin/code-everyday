c85bedcd39424d8bbbf951969a58a2e0f4734884
abiduddin <abidakash456@gmail.com>
2021-12-01 23:57:33 +0600

    dec 1 - generic method wildcard

--- code-java/src/twenty/one/nov/GenericWildCard.java
+++ code-java/src/twenty/one/nov/GenericWildCard.java
@@ -7,10 +7,12 @@ public class GenericWildCard {
     public static void main(String[] args) {
         List<Object> objLst = new ArrayList<Object>();
         objLst.add(33);
+        objLst.add(77);
         print(objLst);
 
         List<String> strLst = new ArrayList<String>();
         strLst.add("one");
+        strLst.add("two");
         print(strLst);
     }
 
