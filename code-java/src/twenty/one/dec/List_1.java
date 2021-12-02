8e7408ff33c377d953fb44ed58b91d5ca022b438
abiduddin <abidakash456@gmail.com>
2021-12-02 23:56:53 +0600

    dec 2 - list

---
+++ code-java/src/twenty/one/dec/List_1.java
@@ -0,0 +1,26 @@
+package twenty.one.dec;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class List_1 {
+
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(7);
+        list.add(5);
+        list.add(13);
+        list.add(17);
+        list.add(3);
+
+        Collections.sort(list);
+
+        list.forEach((x) -> {
+            System.out.println(x);
+        });
+    }
+}
+
