0c7ee46151be4d4e2f9e193c62ff52a16829d475
abiduddin <abidakash456@gmail.com>
2021-12-05 04:15:48 +0600

    dec 5 - thread volatile

---
+++ code-java/src/thread/Thread1.java
@@ -0,0 +1,48 @@
+package thread;
+
+class RunnableImpl implements Runnable {
+
+    @Override
+    public void run() {
+
+        int a = 50;
+
+        while (a-- > 0)
+            System.out.println("run - " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ThreadImpl extends Thread {
+
+    @Override
+    public void run() {
+
+        int a = 50;
+
+        while (a-- > 0)
+            System.out.println("run - " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class Thread1 {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new RunnableImpl());
+        thread.start();
+
+        ThreadImpl thread1 = new ThreadImpl();
+        thread1.start();
+    }
+}
