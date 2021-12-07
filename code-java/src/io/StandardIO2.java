c3c5fadc59918cee311f7e28b3ede33b61dab91e
abiduddin <abidakash456@gmail.com>
2021-12-07 13:01:25 +0600

    dec 7 - update IO2 input in

--- code-java/src/io/StandardIO2.java
+++ code-java/src/io/StandardIO2.java
@@ -1,19 +1,39 @@
 package io;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class StandardIO2 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+
+        startBufferReader();
+        startScanner();
+    }
+
+    private static void startScanner() {
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        do {
+            line = scanner.nextLine();
+            System.out.println(line);
+        } while (!line.equals("quit"));
+    }
+
+    public static void startBufferReader() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String line;
 
-        do {
-            line = reader.readLine();
-            line = line.toLowerCase();
-            System.out.println(line);
-        } while (!line.equals("quit"));
+        try {
+            do {
+                line = reader.readLine();
+                line = line.toLowerCase();
+                System.out.println(line);
+            } while (!line.equals("quit"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
 
