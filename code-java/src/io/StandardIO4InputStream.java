72740259f3e8268ff87e226f835fbf3e41b7a3fc
abiduddin <abidakash456@gmail.com>
2021-12-11 23:53:50 +0600

    dec 11 - update IO files

---
+++ code-java/src/io/StandardIO4InputStream.java
@@ -0,0 +1,29 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class StandardIO4InputStream {
+
+    public static void main(String[] args) {
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream("hello.txt");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                System.out.print(c + ",");
+            }
+        } catch (IOException e) {
+            System.err.println("Could not read file");
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e1) {
+                    System.err.println("Could close input stream");
+                }
+            }
+        }
+    }
+}
