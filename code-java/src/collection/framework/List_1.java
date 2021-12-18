937bc4f3d340d1dc09b5af4fa1db08614ca80a32
abiduddin <abidakash456@gmail.com>
2021-12-18 23:50:18 +0600

    dec 18 - update list

--- code-java/src/collection/framework/List_1.java
+++ code-java/src/collection/framework/List_1.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 public class List_1 {
 
+    static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>();
+
         list.add(7);
         list.add(5);
         list.add(13);
@@ -17,10 +18,30 @@ public class List_1 {
         list.add(3);
 
         Collections.sort(list);
+        printList();
 
+        list.set(2,55);
+        printList();
+
+        list.remove(2);
+        printList();
+
+        list.remove(new Integer(13));
+        printList();
+
+        list.add(3);
+        list.remove(new Integer(3));
+        printList();
+
+
+
+    }
+
+    private static void printList() {
         list.forEach((x) -> {
             System.out.println(x);
         });
+        System.out.println("-------------------");
     }
 }
 
