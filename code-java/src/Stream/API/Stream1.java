cae9fe28ace3a180cfd5bf5eec8d1ebb63db67fa
abiduddin <abidakash456@gmail.com>
2022-01-01 23:52:34 +0600

    jan 1 - stream

--- code-java/src/Stream/API/Stream1.java
+++ code-java/src/Stream/API/Stream1.java
@@ -20,7 +20,22 @@ public class Stream1 {
 
         myStream = myList.stream();
         System.out.println("max value: " + myStream.max(Integer::compare).get());
-        myList.stream().sorted().forEach( n -> System.out.print(n + " "));
+
+        System.out.println("sorted list");
+        myList.stream().sorted().forEach(n -> System.out.print(n + " "));
+        System.out.println();
+
+        Stream<Integer> odds = myList.stream().sorted().filter((n) -> (n % 2) == 1);
+        System.out.println("Odd values: ");
+        odds.forEach((n) -> System.out.print(n + " "));
+        System.out.println();
+
+        odds = myList.stream()
+                .filter(n -> n % 2 == 1)
+                .filter(n -> n > 23);
+        System.out.println("odds number and greater than 23: ");
+        odds.forEach(n -> System.out.print(n + " "));
+
 
     }
 }
