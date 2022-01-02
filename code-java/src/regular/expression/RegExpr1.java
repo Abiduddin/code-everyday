d4f7071a327e5f9866f2e584ccf24592d0b5457c
abiduddin <abidakash456@gmail.com>
2022-01-02 23:48:30 +0600

    jan 2 - regex 1

---
+++ code-java/src/regular/expression/RegExpr1.java
@@ -0,0 +1,35 @@
+package regular.expression;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegExpr1 {
+    public static void main(String[] args) {
+        Pattern pat;
+        Matcher mat;
+        boolean found;
+
+        pat = Pattern.compile("Java");
+        mat = pat.matcher("Java");
+        found = mat.matches();
+
+        System.out.println("Testing Java against Java");
+
+        if (found)
+            System.out.println("Matches");
+        else
+            System.out.println("No Match");
+
+        System.out.println();
+
+        System.out.println("Testing Java against Java SE");
+        mat = pat.matcher("Java SE");
+
+        found = mat.matches();
+
+        if (found)
+            System.out.println("Matches");
+        else
+            System.out.println("No Match");
+    }
+}
