6e158baaaaf27c874b2757123c77946b579ce551
abiduddin <abidakash456@gmail.com>
2022-01-17 01:33:10 +0600

    jan 17 - basic

---
+++ code-java/src/basic/Array1.java
@@ -0,0 +1,13 @@
+package basic;
+
+import java.util.Arrays;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int[] arr = new int[6];
+
+        System.out.println("array length: "+arr.length);
+        System.out.println("array elements: ");
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
