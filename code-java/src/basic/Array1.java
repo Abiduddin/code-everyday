ceca3139e2267da4bef76dcc27993e92c4b0f90f
abiduddin <abidakash456@gmail.com>
2022-01-31 23:54:43 +0600

    jan 31 - demo vue

--- code-java/src/basic/Array1.java
+++ code-java/src/basic/Array1.java
@@ -6,7 +6,7 @@ public class Array1 {
     public static void main(String[] args) {
         int[] arr = new int[6];
 
-        System.out.println("array length: "+arr.length);
+        System.out.println("array length: " + arr.length);
         System.out.println("array elements: ");
         Arrays.stream(arr).forEach(System.out::println);
     }
